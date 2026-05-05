77a551a25d3956dce074cf287446581671f5fbc6
HaroldXL <kalkelvinxl@gmail.com>
2026-05-05 23:42:15 +0700

    Fix api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://engconnect-qa.gdev.id.vn/api/",
+  baseURL: "https://engconnect-prod.gdev.id.vn/api/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
